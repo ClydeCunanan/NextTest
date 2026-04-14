4efe90b4f8eed193e35e0699db8700cad543f100
SanaPasado <cscunanan@student.hau.edu.ph>
2026-04-14 19:27:54 +0800

    moved use client on top of import

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   UserGroupIcon,
   HomeIcon,
@@ -7,7 +8,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-'use client';
+
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
